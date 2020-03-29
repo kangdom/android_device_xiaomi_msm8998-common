0efe4583d20c788ad78be3d4ba341e4c601ce630
EnderXH <enderhx5@gmail.com>
2020-03-29 19:24:15 +0500

    sagit: small fixes

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,9 +24,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+HAVOC_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${HAVOC_ROOT}/vendor/havoc/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
